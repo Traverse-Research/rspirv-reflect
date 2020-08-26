270d216f3ec2ead85b61e73de16ae9aa2a7a6ccc
Marijn Suijten <marijns95@gmail.com>
2020-08-26 09:40:58 +0200

    tests: Use PartialEq on DescriptorInfo

--- tests/hlsl.rs
+++ tests/hlsl.rs
@@ -23,39 +23,84 @@ fn hlsl_bindings() {
     let set5 = &sets[&5];
     let set6 = &sets[&6];
 
-    assert_eq!(set0[&0].name, "g_input");
-    assert_eq!(set0[&0].ty, DescriptorType::STORAGE_BUFFER);
-    assert_eq!(set0[&0].is_bindless, false);
+    assert_eq!(
+        set0[&0],
+        DescriptorInfo {
+            name: "g_input".to_string(),
+            ty: DescriptorType::STORAGE_BUFFER,
+            is_bindless: false
+        }
+    );
 
-    assert_eq!(set0[&1].name, "g_output");
-    assert_eq!(set0[&1].ty, DescriptorType::STORAGE_BUFFER);
-    assert_eq!(set0[&1].is_bindless, false);
+    assert_eq!(
+        set0[&1],
+        DescriptorInfo {
+            name: "g_output".to_string(),
+            ty: DescriptorType::STORAGE_BUFFER,
+            is_bindless: false
+        }
+    );
 
-    assert_eq!(set0[&2].name, "g_constant");
-    assert_eq!(set0[&2].ty, DescriptorType::UNIFORM_BUFFER);
-    assert_eq!(set0[&2].is_bindless, false);
+    assert_eq!(
+        set0[&2],
+        DescriptorInfo {
+            name: "g_constant".to_string(),
+            ty: DescriptorType::UNIFORM_BUFFER,
+            is_bindless: false
+        }
+    );
 
-    assert_eq!(set1[&0].name, "g_bindlessInput");
-    assert_eq!(set1[&0].ty, DescriptorType::STORAGE_BUFFER);
-    assert_eq!(set1[&0].is_bindless, true);
+    assert_eq!(
+        set1[&0],
+        DescriptorInfo {
+            name: "g_bindlessInput".to_string(),
+            ty: DescriptorType::STORAGE_BUFFER,
+            is_bindless: true
+        }
+    );
 
-    assert_eq!(set2[&0].name, "g_texture2d");
-    assert_eq!(set2[&0].ty, DescriptorType::SAMPLED_IMAGE);
-    assert_eq!(set2[&0].is_bindless, false);
+    assert_eq!(
+        set2[&0],
+        DescriptorInfo {
+            name: "g_texture2d".to_string(),
+            ty: DescriptorType::SAMPLED_IMAGE,
+            is_bindless: false
+        }
+    );
 
-    assert_eq!(set3[&0].name, "g_rwtexture2d");
-    assert_eq!(set3[&0].ty, DescriptorType::STORAGE_IMAGE);
-    assert_eq!(set3[&0].is_bindless, false);
+    assert_eq!(
+        set3[&0],
+        DescriptorInfo {
+            name: "g_rwtexture2d".to_string(),
+            ty: DescriptorType::STORAGE_IMAGE,
+            is_bindless: false
+        }
+    );
 
-    assert_eq!(set4[&0].name, "g_bindlessrwtexture2d");
-    assert_eq!(set4[&0].ty, DescriptorType::STORAGE_IMAGE);
-    assert_eq!(set4[&0].is_bindless, true);
+    assert_eq!(
+        set4[&0],
+        DescriptorInfo {
+            name: "g_bindlessrwtexture2d".to_string(),
+            ty: DescriptorType::STORAGE_IMAGE,
+            is_bindless: true
+        }
+    );
 
-    assert_eq!(set5[&0].name, "g_sampler");
-    assert_eq!(set5[&0].ty, DescriptorType::SAMPLER);
-    assert_eq!(set5[&0].is_bindless, false);
+    assert_eq!(
+        set5[&0],
+        DescriptorInfo {
+            name: "g_sampler".to_string(),
+            ty: DescriptorType::SAMPLER,
+            is_bindless: false
+        }
+    );
 
-    assert_eq!(set6[&0].name, "g_byteAddressBuffer");
-    assert_eq!(set6[&0].ty, DescriptorType::STORAGE_BUFFER);
-    assert_eq!(set6[&0].is_bindless, true);
+    assert_eq!(
+        set6[&0],
+        DescriptorInfo {
+            name: "g_byteAddressBuffer".to_string(),
+            ty: DescriptorType::STORAGE_BUFFER,
+            is_bindless: true
+        }
+    );
 }
