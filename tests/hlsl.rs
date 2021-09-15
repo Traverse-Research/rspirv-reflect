Traverse-Research/rspirv-reflect

commit f2abc0950c68ba1a8ab889c0029fe53900e9b463

implemented a test for push constants

src/lib.rs
@@ -350,7 +350,6 @@ impl Reflection {
 
         // return the value from the last offset member decoration
         for i in member_decorates.iter().rev() {
-            debug_assert!(i.operands.len() == 4);
             let decoration = get_operand_at!(**i, Operand::Decoration, 2)?;
             if let spirv::Decoration::Offset = decoration {
                 return Ok(get_operand_at!(**i, Operand::LiteralInt32, 3)?);

tests/hlsl.rs
@@ -104,3 +104,16 @@ fn hlsl_bindings() {
         }
     );
 }
+
+#[test]
+fn push_constants() {
+    let spirv = include_bytes!("push_constant_tests_ps-hlsl.spv");
+    let reflect = Reflection::new_from_spirv(spirv)
+        .expect("Failed to create reflection module from spirv code");
+    let range = reflect
+        .get_push_constant_range()
+        .expect("failed to extract push constants")
+        .expect("defined push constants not detected");
+
+    assert_eq!(range.size, 404);
+}

tests/compile_shaders.sh
@@ -4,7 +4,12 @@ DXC=${DXC:-dxc}
 
 current_dir=$(dirname ${BASH_SOURCE[0]})
 
-for hlsl in $current_dir/*.hlsl; do
+for hlsl in $current_dir/*ps.hlsl; do
+    spirv=${hlsl%.hlsl}-hlsl.spv
+    ${DXC} -E main -T ps_6_5 -spirv -fvk-use-scalar-layout $hlsl -Fo $spirv
+done
+
+for hlsl in $current_dir/*cs.hlsl; do
     spirv=${hlsl%.hlsl}-hlsl.spv
     ${DXC} -E main -T cs_6_5 -spirv -fvk-use-scalar-layout $hlsl -Fo $spirv
 done

tests/push_constant_tests_ps.hlsl
@@ -1,0 +1,17 @@
+struct PSInput {
+    float4 color : COLOR;
+};
+
+struct PushConstant {
+    uint a;
+    float b;
+    bool c;
+    uint64_t d;
+    float64_t e[6];
+    float4x4 f[5];
+    vector<int, 3> g;
+};
+
+[[vk::push_constant]] PushConstant p;
+
+float4 main(PSInput input) : SV_TARGET { return input.color * p.a; }

commit db1fadf2a40a2db910a4d779c163f952ec7609ec

Explicitly provide the number of descriptors in a binding (#17)
* tests: Fix the _cs test shader not using the recompiled variant

Fixes: f2abc09 ("implemented a test for push constants")

* Static-sized arrays of bindings should not receive is_bindless flag

* Explicitly provide the number of descriptors in a binding

src/lib.rs
@@ -7,6 +7,8 @@
 use rspirv::binary::Parser;
 use rspirv::dr::{Instruction, Loader, Module, Operand};
 use std::collections::HashMap;
+use std::convert::TryInto;
+use std::num::TryFromIntError;
 use thiserror::Error;
 
 pub use rspirv;
@@ -49,6 +51,10 @@ pub enum ReflectError {
     TooManyPushConstants,
     #[error("SPIR-V parse error")]
     ParseError(#[from] rspirv::binary::ParseState),
+    #[error("OpTypeInt cannot have width {0}")]
+    UnexpectedIntWidth(u32),
+    #[error(transparent)]
+    TryFromIntError(#[from] TryFromIntError),
 }
 
 type Result<V, E = ReflectError> = ::std::result::Result<V, E>;
@@ -101,9 +107,36 @@ impl std::fmt::Debug for DescriptorType {
 }
 
 #[derive(Debug, Clone, PartialEq)]
+pub enum BindingCount {
+    /// A single resource binding.
+    ///
+    /// # Example
+    /// ```hlsl
+    /// StructuredBuffer<uint>
+    /// ```
+    One,
+    /// Predetermined number of resource bindings.
+    ///
+    /// # Example
+    /// ```hlsl
+    /// StructuredBuffer<uint> myBinding[4]
+    /// ```
+    StaticSized(usize),
+    /// Variable number of resource bindings (usually dubbed "bindless").
+    ///
+    /// Count is determined in `vkDescriptorSetLayoutBinding`. No other bindings should follow in this set.
+    ///
+    /// # Example
+    /// ```hlsl
+    /// StructuredBuffer<uint> myBinding[]
+    /// ```
+    Unbounded,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct DescriptorInfo {
     pub ty: DescriptorType,
-    pub is_bindless: bool,
+    pub binding_count: BindingCount,
     pub name: String,
 }
 
@@ -203,10 +236,34 @@ impl Reflection {
 
         // Weave with recursive types
         match type_instruction.class.opcode {
-            spirv::Op::TypeArray | spirv::Op::TypeRuntimeArray => {
+            spirv::Op::TypeArray => {
+                let element_type_id = get_operand_at!(type_instruction, Operand::IdRef, 0)?;
+                let num_elements_id = get_operand_at!(type_instruction, Operand::IdRef, 1)?;
+                let num_elements =
+                    Self::find_assignment_for(&self.0.types_global_values, num_elements_id)?;
+                assert_eq!(num_elements.class.opcode, spirv::Op::Constant);
+                let num_elements_ty = Self::find_assignment_for(
+                    &self.0.types_global_values,
+                    num_elements.result_type.unwrap(),
+                )?;
+                // Array size can be any width, any signedness
+                assert_eq!(num_elements_ty.class.opcode, spirv::Op::TypeInt);
+                let num_elements = match get_operand_at!(num_elements_ty, Operand::LiteralInt32, 0)?
+                {
+                    32 => get_operand_at!(num_elements, Operand::LiteralInt32, 0)?.try_into()?,
+                    64 => get_operand_at!(num_elements, Operand::LiteralInt64, 0)?.try_into()?,
+                    x => return Err(ReflectError::UnexpectedIntWidth(x)),
+                };
+                assert!(num_elements >= 1);
+                return Ok(DescriptorInfo {
+                    binding_count: BindingCount::StaticSized(num_elements),
+                    ..self.get_descriptor_type_for_var(element_type_id, storage_class)?
+                });
+            }
+            spirv::Op::TypeRuntimeArray => {
                 let element_type_id = get_operand_at!(type_instruction, Operand::IdRef, 0)?;
                 return Ok(DescriptorInfo {
-                    is_bindless: true,
+                    binding_count: BindingCount::Unbounded,
                     ..self.get_descriptor_type_for_var(element_type_id, storage_class)?
                 });
             }
@@ -328,7 +385,7 @@ impl Reflection {
 
         Ok(DescriptorInfo {
             ty: descriptor_type,
-            is_bindless: false,
+            binding_count: BindingCount::One,
             name: "".to_string(),
         })
     }

tests/hlsl.rs
@@ -2,7 +2,7 @@ use rspirv_reflect::*;
 
 #[test]
 fn hlsl_bindings() {
-    let spirv = include_bytes!("shader-hlsl.spv");
+    let spirv = include_bytes!("shader_cs-hlsl.spv");
 
     let reflect = Reflection::new_from_spirv(spirv)
         .expect("Failed to create reflection module from spirv code");
@@ -22,13 +22,15 @@ fn hlsl_bindings() {
     let set4 = &sets[&4];
     let set5 = &sets[&5];
     let set6 = &sets[&6];
+    let set7 = &sets[&7];
+    let set8 = &sets[&8];
 
     assert_eq!(
         set0[&0],
         DescriptorInfo {
             name: "g_input".to_string(),
             ty: DescriptorType::STORAGE_BUFFER,
-            is_bindless: false
+            binding_count: BindingCount::One
         }
     );
 
@@ -37,7 +39,7 @@ fn hlsl_bindings() {
         DescriptorInfo {
             name: "g_output".to_string(),
             ty: DescriptorType::STORAGE_BUFFER,
-            is_bindless: false
+            binding_count: BindingCount::One
         }
     );
 
@@ -46,7 +48,7 @@ fn hlsl_bindings() {
         DescriptorInfo {
             name: "g_constant".to_string(),
             ty: DescriptorType::UNIFORM_BUFFER,
-            is_bindless: false
+            binding_count: BindingCount::One
         }
     );
 
@@ -55,7 +57,7 @@ fn hlsl_bindings() {
         DescriptorInfo {
             name: "g_bindlessInput".to_string(),
             ty: DescriptorType::STORAGE_BUFFER,
-            is_bindless: true
+            binding_count: BindingCount::Unbounded
         }
     );
 
@@ -64,7 +66,7 @@ fn hlsl_bindings() {
         DescriptorInfo {
             name: "g_texture2d".to_string(),
             ty: DescriptorType::SAMPLED_IMAGE,
-            is_bindless: false
+            binding_count: BindingCount::One
         }
     );
 
@@ -73,7 +75,7 @@ fn hlsl_bindings() {
         DescriptorInfo {
             name: "g_rwtexture2d".to_string(),
             ty: DescriptorType::STORAGE_IMAGE,
-            is_bindless: false
+            binding_count: BindingCount::One
         }
     );
 
@@ -82,7 +84,7 @@ fn hlsl_bindings() {
         DescriptorInfo {
             name: "g_bindlessrwtexture2d".to_string(),
             ty: DescriptorType::STORAGE_IMAGE,
-            is_bindless: true
+            binding_count: BindingCount::Unbounded
         }
     );
 
@@ -91,7 +93,7 @@ fn hlsl_bindings() {
         DescriptorInfo {
             name: "g_sampler".to_string(),
             ty: DescriptorType::SAMPLER,
-            is_bindless: false
+            binding_count: BindingCount::One
         }
     );
 
@@ -100,7 +102,43 @@ fn hlsl_bindings() {
         DescriptorInfo {
             name: "g_byteAddressBuffer".to_string(),
             ty: DescriptorType::STORAGE_BUFFER,
-            is_bindless: true
+            binding_count: BindingCount::Unbounded
+        }
+    );
+
+    assert_eq!(
+        set7[&0],
+        DescriptorInfo {
+            name: "g_rwbyteAddressBuffer".to_string(),
+            ty: DescriptorType::STORAGE_BUFFER,
+            binding_count: BindingCount::One
+        }
+    );
+
+    assert_eq!(
+        set8[&0],
+        DescriptorInfo {
+            name: "g_inputArray".to_string(),
+            ty: DescriptorType::STORAGE_BUFFER,
+            binding_count: BindingCount::One
+        }
+    );
+
+    assert_eq!(
+        set8[&1],
+        DescriptorInfo {
+            name: "g_arrayOfInputs".to_string(),
+            ty: DescriptorType::STORAGE_BUFFER,
+            binding_count: BindingCount::StaticSized(4)
+        }
+    );
+
+    assert_eq!(
+        set8[&6],
+        DescriptorInfo {
+            name: "g_bindlessInputArray".to_string(),
+            ty: DescriptorType::STORAGE_BUFFER,
+            binding_count: BindingCount::Unbounded
         }
     );
 }

tests/shader_cs.hlsl
@@ -14,6 +14,10 @@ SamplerState g_sampler : register(s0, space5);
 ByteAddressBuffer g_byteAddressBuffer[] : register(t0, space6);
 RWByteAddressBuffer g_rwbyteAddressBuffer : register(u0, space7);
 
+StructuredBuffer<uint[4]> g_inputArray : register(t0, space8);
+StructuredBuffer<uint> g_arrayOfInputs[4] : register(t1, space8);
+StructuredBuffer<uint[4]> g_bindlessInputArray[] : register(t6, space8);
+
 static const uint s_constant = 34;
 
 [numthreads(64, 1, 1)]
@@ -27,5 +31,9 @@ void main(int threadId: SV_DispatchThreadID)
         + g_rwtexture2d.Load(int2(0, 0)).x
         + g_bindlessrwtexture2d[threadId].Load(int2(0, 0)).x
         + (uint)g_texture2d.SampleLevel(g_sampler, int2(0, 0), 0.0).x
-        + g_byteAddressBuffer[threadId].Load(0).x;
+        + g_byteAddressBuffer[threadId].Load(0).x
+        + g_rwbyteAddressBuffer.Load(0)
+        + g_inputArray[threadId][3]
+        + g_arrayOfInputs[3][threadId]
+        + g_bindlessInputArray[threadId][0][3];
 }